2e56dc8b340c3ada50b13b2a61e4e0e09e101b1a
Seung Min Cha <sue7020@naver.com>
2021-05-16 00:03:33 +0900

    05.15 solved

--- src/BoJ/DFS_4963.java
+++ src/BoJ/DFS_4963.java
@@ -27,6 +27,7 @@ public class DFS_4963 {
                     map[i][j] = sc.nextInt();
             }
 
+            // DFS
             for(int i=0; i<h; i++) {
                 for(int j=0; j<w; j++) {
                     if(map[i][j] == 1) {
@@ -36,10 +37,49 @@ public class DFS_4963 {
                 }
             }
 
+            // BFS
+            for(int i=0; i<h; i++) {
+                for(int j=0; j<w; j++) {
+                    if(map[i][j] == 1) {
+                        bfs(i, j, map);
+                        answer++;
+                    }
+                }
+            }
+
             System.out.println(answer);
 
         }
 
+    }
+
+    private static void bfs(int i, int j, int[][] map) {
+        Queue<Integer> que_x = new LinkedList<>();
+        Queue<Integer> que_y = new LinkedList<>();
+
+        que_x.add(i);
+        que_y.add(j);
+        map[i][j] = 0;
+
+        while(!que_x.isEmpty() && !que_y.isEmpty())
+        {
+            int x = que_x.remove();
+            int y = que_y.remove();
+
+            for(int k=0; k<dx.length; k++)
+            {
+                int xx = x+dx[k];
+                int yy = y+dy[k];
+
+
+                if(check(xx, yy) && map[xx][yy] == 1) {
+
+                    map[xx][yy] = 0;
+                    que_x.add(xx);
+                    que_y.add(yy);
+                }
+            }
+        }
     }
 
     private static void dfs(int x, int y, int[][] map) {
