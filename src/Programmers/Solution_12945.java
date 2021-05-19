10d7a879730ac044ce22fea61d0118287768270d
Seung Min Cha <sue7020@naver.com>
2021-05-19 22:28:19 +0900

    05.19 solved

---
+++ src/Programmers/Solution_12945.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class Solution_12945 {
+    public static void main(String[] args) {
+        int n = 5;
+        solution(n);
+    }
+
+    private static void solution(int n) {
+        int answer;
+        int[] d = new int[n+1];
+
+        d[0] = 0;
+        d[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            d[i] = (d[i-1]+d[i-2])%1234567;
+        }
+
+        answer = d[n];
+        System.out.println(answer);
+    }
+}
